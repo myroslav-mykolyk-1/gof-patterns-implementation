285eaae4803e1a3b975a2acb1dc47aebccf8b1dc
Myroslav Mykolyk <myroslav.mykolyk@imprivata.com>
2025-02-20 14:33:16 +0200

    Added some improvements to the implementation of Singleton pattern

--- src/com/mykolyk/creational/singleton/Main.java
+++ src/com/mykolyk/creational/singleton/Main.java
@@ -3,8 +3,10 @@ package com.mykolyk.creational.singleton;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Logger.getLogger().toString());
-        Logger.getLogger().addLogInfo("Singleton check.");
+        Logger.getLogger().addLogInfo("Log message 1.");
+        Logger.getLogger().showLogInfo();
         System.out.println(Logger.getLogger().toString());
+        Logger.getLogger().addLogInfo("Log message 2.");
         Logger.getLogger().showLogInfo();
     }
 }
